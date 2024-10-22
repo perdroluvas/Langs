c7acaf5f9e0bf1d2165d42cf6d786c33351455af
Perdro <plbsa.dev@gmail.com>
2024-10-21 23:09:48 -0300

    Initial commit

---
+++ TYPESCRIPT_PROJS/index.js
@@ -0,0 +1,5 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+var result = addNumbers(5, 10);
+console.log("The sum is: ".concat(result));
---
+++ TYPESCRIPT_PROJS/index.ts
@@ -0,0 +1,7 @@
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+const result = addNumbers(5, 10);
+console.log(`The sum is: ${result}`);
+
